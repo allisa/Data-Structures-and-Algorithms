62d438644760dbd6f13323fb016626de77845b6a
Allisa LeBeuf <al5906@gmail.com>
2018-10-09 22:25:48 -0700

    created test arrays and prepared console.write statements

---
+++ Challenges/arrayShift/arrayShift/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace arrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating test arrays
+            int[] testArray = { 2, 4, 6, 9 };
+            int[] testArray2 = { 4, 8, 23, 42 };
+
+            //printing arrays with value in the middle index
+            Console.Write(string.Join(",", ArrayShift(testArray, 5)));
+            Console.Write(string.Join(",", ArrayShift(testArray2, 16)));
+        }
+
+        //mathod to take original array, find the middle, and insert
+        //the value into the center of the new array and return new array
+        static int[] ArrayShift(int[], int value)
+
+
+    }
+}
