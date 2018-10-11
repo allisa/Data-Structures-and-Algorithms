3fcbff56e350c30b7e5ec1b8f0f2560baf899e5d
Allisa LeBeuf <al5906@gmail.com>
2018-10-11 09:13:53 -0700

    initial file setup

---
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray1 = { 4, 8, 15, 16, 23, 42 };
+            int[] testArray2 = {11, 22, 33, 44, 55, 66, 77}
+        }
+    }
+}
