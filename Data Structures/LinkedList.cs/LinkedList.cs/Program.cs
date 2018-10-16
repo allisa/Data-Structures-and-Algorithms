48603ce16975e22220140dda058bf89de768cd5a
Allisa LeBeuf <al5906@gmail.com>
2018-10-15 18:19:21 -0700

    added addafter method

---
+++ Data Structures/LinkedList.cs/LinkedList.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //demo code
+            //Node node1 = new Node(7);
+            //Node node2 = new Node("cat");
+
+            //LList myAwesomeList = new LList(node1);
+            //myAwesomeList.Append(node2);
+
+            //myAwesomeList.Print();
+
+        }
+    }
+}
