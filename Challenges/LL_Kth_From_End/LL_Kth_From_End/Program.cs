11c9f520ac30c4c4c4594741e616e58f4208b5af
Allisa LeBeuf <al5906@gmail.com>
2018-10-17 00:04:43 -0700

    find node method added

---
+++ Challenges/LL_Kth_From_End/LL_Kth_From_End/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LL_Kth_From_End
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/LL_Kth_From_End/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LL_Kth_From_End;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
