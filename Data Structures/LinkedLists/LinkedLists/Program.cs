eb9276c7090d1284196c2f9552980dfb6cde1b7e
Allisa LeBeuf <al5906@gmail.com>
2018-10-17 10:14:35 -0700

    update to code

--- Data Structures/LinkedLists/LinkedLists/Program.cs
+++ Data Structures/LinkedLists/LinkedLists/Program.cs
@@ -11,8 +11,11 @@ namespace LinkedLists
 
                 Node node1 = new Node(7);
                 Node node2 = new Node(40);
+                Node node3 = new Node("dog");
+                Node node4 = new Node(33);
+                Node node5 = new Node("button");
 
-                LList list = new LList(node1);
+            LList list = new LList(node1);
                 list.Append(node2);
 
                 list.Print();
