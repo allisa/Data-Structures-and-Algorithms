d48d2b0bdc2c6153c5d933e621c41778f8887c13
Allisa LeBeuf <al5906@gmail.com>
2018-10-22 22:40:12 -0700

    most code in, queue constructor not working

--- Challenges/QueueWithStacks/QueueWithStacks/Program.cs
+++ Challenges/QueueWithStacks/QueueWithStacks/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Stack_and_Queue.Classes;
+using QueueWithStacks.Classes;
 
 namespace QueueWithStacks
 {
@@ -7,8 +9,24 @@ namespace QueueWithStacks
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
 
-        public static void 
+            Stack stack1 = new Stack(null);
+            Stack stack2 = new Stack(null);
+
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+
+            QueueofStacks queue = new QueueofStacks(stack1, stack2);
+
+            queue.Enqueue(node1);
+            queue.Enqueue(node2);
+            queue.Enqueue(node3);
+
+
+            Console.WriteLine($"Last item added: {queue.stack1.Top.Value}");
+
+            Console.WriteLine($"Item dequeued: {queue.Dequeue().Value}");
+        }
     }
 }
