56a593d948b3c967c3c381dd61ed84ed59b28e8f
Allisa LeBeuf <al5906@gmail.com>
2018-10-22 21:02:36 -0700

    file setup

---
+++ Challenges/QueueWithStacks/QueueWithStacks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QueueWithStacks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void 
+    }
+}
