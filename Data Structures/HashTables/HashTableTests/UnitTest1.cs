3cd76b61f31f74a123bd9951da43e72bf620583b
Allisa LeBeuf <al5906@gmail.com>
2018-11-12 18:53:56 -0800

    initial file setup

---
+++ Data Structures/HashTables/HashTableTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using HashTables;
+
+namespace HashTableTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Data Structures/HashTables/HashTables/Classes/HashTable.cs
@@ -0,0 +1,13 @@
+﻿using LinkedLists.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HashTables.Classes
+{
+    class HashTable
+    {
+        LList[] Table = new LList[1024];
+    }
+}
