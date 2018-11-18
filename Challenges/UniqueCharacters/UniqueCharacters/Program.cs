4deb075f49e2bb9e6888bf9a4a69386fcc370c0b
Allisa LeBeuf <al5906@gmail.com>
2018-11-18 15:51:41 -0800

    file setup complete

---
+++ Challenges/UniqueCharacters/UniqueCharacters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace UniqueCharacters
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool UniqueChar(string input)
+        {
+            Hashtable hashTable = new HashTable();
+        }
+    }
+}
