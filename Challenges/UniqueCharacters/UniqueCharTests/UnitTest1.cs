127c68b7b2af79897c0df378ca4ab6636d4ab1f5
Allisa LeBeuf <al5906@gmail.com>
2018-11-18 19:34:31 -0800

    tests added and passing

--- Challenges/UniqueCharacters/UniqueCharTests/UnitTest1.cs
+++ Challenges/UniqueCharacters/UniqueCharTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using UniqueCharacters;
 
 namespace UniqueCharTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        /// <summary>
+        /// test to look for unique characters in strings
+        /// </summary>
+        /// <param name="testString"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("Hello World", false)]
+        [InlineData("asdfg hjkl zxcvb nmqwer", true)]
+        [InlineData("Wizard world", false)]
+        public void TestUniqueCharacters(string testString, bool expected)
         {
-
+            Assert.Equal(expected, Program.UniqueChar(testString));
         }
     }
 }
